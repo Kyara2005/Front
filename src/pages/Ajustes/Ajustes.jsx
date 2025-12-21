700f842ef95c01f27f6423c638652eb7b766001a
Damaris Lopez <damaris.lopez@epn.edu.ec>
2025-12-21 18:17:34 -0500

    Update Ajustes.jsx

--- src/pages/Ajustes/Ajustes.jsx
+++ src/pages/Ajustes/Ajustes.jsx
@@ -26,7 +26,7 @@ const Ajustes = () => {
         if (!token || !import.meta.env.VITE_BACKEND_URL) return;
 
         const res = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/api/perfil`,
+          `${import.meta.env.VITE_BACKEND_URL}/usuarios/perfil`,
           { headers: { Authorization: `Bearer ${token}` } }
         );
 
